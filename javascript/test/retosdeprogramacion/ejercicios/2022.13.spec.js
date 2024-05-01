725393c461e09d675a5df3dcc3e13f40bd4278e4
alfonsolab <alfonso.cobelas@gmail.com>
2024-05-01 15:05:01 +0200

    solve 2022.13

---
+++ javascript/test/retosdeprogramacion/ejercicios/2022.13.spec.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai'
+import { factorial } from '../../../retosdeprogramacion/ejercicios/2022.13.solve.js'
+
+describe('retosdeprogramacion/ejercicios/2022.08 unit-test', () => {
+    describe('factorial()', () => {
+        it('case 1', () => {
+            expect( factorial(0) ).to.eq(1)
+        })
+    
+        it('case 2', () => {
+            expect( factorial(7) ).to.eq(5040)
+        })
+    
+        it('case 3', () => {
+            expect( factorial(10) ).to.eq(3628800)
+        })
+
+        it('case 4', () => {
+            expect( factorial(1) ).to.eq(1)
+        })
+            
+        it('case 5', () => {
+            try {
+                factorial(-1)
+            } catch(err) {
+                expect(err.message).to.equal('Error: número negativo')
+            }
+        })
+    })
+})
